63f0c79baa846c145436fc737cf9b8b5272f58f6
UnDeadCharger <rexfury121@gmail.com>
2021-04-02 23:30:29 +0700

    Added color and function options to list
    there are now options to pick

--- function.cpp
+++ function.cpp
@@ -38,21 +38,51 @@ void gotoxy(int x, int y)
     }
 void displayUI()
     {
-		int options;
-		cin >> options;
+        int staffFlag = 1;
+        int options;
         do
         {
+           
             system("CLS");
+            HANDLE  hConsole;
+            hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
             createframe();
-
+            SetConsoleTextAttribute(hConsole, 121);
+            gotoxy(20, 4); cout << "1. Profile";
+            gotoxy(20, 5); cout << "2. Change password"; // can be added later on
+            if (staffFlag == 1) // chừng nào check là staff được thì sẽ sửa.
+            {   
+                gotoxy(20, 3); cout << "Welcome Staff";
+                gotoxy(20, 6); cout << "3. Create SchoolYear";
+                gotoxy(20, 7); cout << "0. Logout";
+            }
+            else
+            {
+                gotoxy(20, 3); cout << "Welcome Students";
+                gotoxy(20, 6); cout << "0. Logout";
+            }
+            SetConsoleTextAttribute(hConsole, 6);
+            gotoxy(0, 11); cout << " >> Please, select your functions: ";
             cin >> options;
         } while (options != 0);
     }
-void createframe()
+    void createframe()
         {
-
-        int x = 60;
+        
+        int x = 60;  // change size of input
         int y = 10;
+        HANDLE  hConsole;
+        hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+        SetConsoleTextAttribute(hConsole, 119);
+        for (int i = 0; i < y; i++)
+        {
+            for (int j = 0; j < x; j++)
+            {
+                cout << " " ;
+            }
+            cout << endl;
+        }
+        SetConsoleTextAttribute(hConsole, 158);
         for (int i = 1; i < x; i++) // tao dong ngang
         {
             gotoxy(i, 0); printf("%c", 205); // dong ngang tren
@@ -67,5 +97,6 @@ void createframe()
         gotoxy(x, y); printf("%c", 188); // cai moc cau ben phai ben duoi
         gotoxy(0, 0); printf("%c", 201); // cai moc cau ben trai tren
         gotoxy(0, y); printf("%c", 200); // moc cau ben trai duoi
+        gotoxy(26, 0); cout << "  MENU  ";
         cout << endl;
      }
