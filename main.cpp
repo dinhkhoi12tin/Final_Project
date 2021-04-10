044717c06cae0abda4ae28001fca715cbd7f54f4
TQAQuoc <79797296+20127304-AQ@users.noreply.github.com>
2021-04-10 18:34:30 +0700

    Fix problem of menu

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include"Header.h"
 int main()
 {
-   // Menu_Feature_First();
-    Option();
-   
+    Menu();
+    /*Students* stu = nullptr;
+    Students* sta = nullptr;
+    Students* stu_cur = nullptr;
+    string user, pass;
+    cin >> user >> pass;
+    //string filenameStu = "STUDENT_DATABASE.csv";
+    string filenameSta = "STAFF-CSV-DATABASE.csv";
+    //LoadFileStudents(stu, filenameStu);
+    LoadFileStaff(sta, filenameSta);
+    if (CheckUser(stu, stu_cur, user, pass) == true )
+    {
+        cout << stu_cur->password;
+    }*/
     return 0;
 }
-
