d0d7b85d52ed0896394538d5495e4410ee9dfeba
SentoGG <80612446+SentoGG@users.noreply.github.com>
2021-04-13 21:01:16 +0700

    them ham design khung, check giup nha, sai gi de tui sua lai

--- function1.cpp
+++ function1.cpp
@@ -9,7 +9,6 @@ Class* Find(Class* classhead, string classname) {
         classhead = classhead->Next;
     }
     return 0;
-
 }
 void add_one_by_one(Class*& classhead) {
     while (1) {
@@ -146,4 +145,69 @@ void Menu_Feature_First_Staff(Students* stu) {
     }
     Export_new(classhead,stu);
  
+}
+inline void displayUI()
+{
+        int offset = 30;
+        int staffFlag = 1;
+        int options;
+        do
+        {
+            system("CLS");
+            HANDLE  hConsole;
+            hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+            createframe();
+            SetConsoleTextAttribute(hConsole, 121);
+            gotoxy(20 + offset, 4); cout << "1. create class";
+            gotoxy(20 + offset, 5); cout << "2. add one by one student to class"; // can be added later on
+            if (staffFlag == 1) // chừng nào check là staff được thì sẽ sửa.
+            {
+                gotoxy(20 + offset, 3); cout << "Welcome Staff";
+                gotoxy(20 + offset, 6); cout << "3. add by file csv";
+                gotoxy(20 + offset, 7); cout << "0. exit";
+            }
+            else
+            {
+                gotoxy(20 + offset, 3); cout << "Welcome Students";
+                gotoxy(20 + offset, 6); cout << "0. exit";
+            }
+            SetConsoleTextAttribute(hConsole, 6);
+            gotoxy(0 + offset, 11); cout << " >> Please, select your functions: ";
+            cin >> options;
+        } while (options != 0);
+}
+inline void createframe()
+{
+    int offset = 30;
+    int x = 60 + offset;  // change size of input
+    int y = 10;
+    HANDLE  hConsole;
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, 119);
+    for (int i = 0; i < y; i++)
+    {
+        for (int j = 0 + offset; j < x; j++)
+        {
+            gotoxy(j, i);
+            cout << " ";
+        }
+        cout << endl;
+    }
+    SetConsoleTextAttribute(hConsole, 158);
+    for (int i = 0 + offset; i < x; i++) // tao dong ngang
+    {
+        gotoxy(i, 0); printf("%c", 205); // dong ngang tren
+        gotoxy(i, y); printf("%c", 205);  // dong ngang duoi
+    }
+    for (int i = 1; i < y; i++)  // dong ke thang dung
+    {
+        gotoxy(0 + offset, i); printf("%c", 186); // dong thang ben trai
+        gotoxy(x, i); printf("%c", 186); // dong thang ben phai
+    }
+    gotoxy(x, 0); printf("%c", 187); // cai moc cau ben phai ben tre^n
+    gotoxy(x, y); printf("%c", 188); // cai moc cau ben phai ben duoi
+    gotoxy(0 + offset, 0); printf("%c", 201); // cai moc cau ben trai tren
+    gotoxy(0 + offset, y); printf("%c", 200); // moc cau ben trai duoi
+    gotoxy(26 + offset, 0); cout << "  MENU  ";
+    cout << endl;
 }
