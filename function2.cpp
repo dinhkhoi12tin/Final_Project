d46f66ab1d2d9ffd05d5275ca86348d76d550c01
TQAQuoc <79797296+20127304-AQ@users.noreply.github.com>
2021-04-18 12:47:54 +0700

    Edit create year

--- function2.cpp
+++ function2.cpp
@@ -8,25 +8,21 @@ Year* Find_Year(Year* yearh, string name_year)
     return nullptr;
 }
 
-Course* Find_Course(Year*& year_cur, Course* courseh, string name_course, int num_sem)
+Course* Find_Course(Year*& year_cur, Course* courseh, string id_course, int num_sem)
 {
     while (year_cur->sem[num_sem - 1].courseh != nullptr)
     {
-        if (year_cur->sem[num_sem - 1].courseh->course_name == name_course) return year_cur->sem[num_sem - 1].courseh;
+        if (year_cur->sem[num_sem - 1].courseh->course_id == id_course) return year_cur->sem[num_sem - 1].courseh;
         year_cur->sem[num_sem - 1].courseh = year_cur->sem[num_sem - 1].courseh->Next;
     }
     return nullptr;
 }
+
 void Menu_Op()
 {
     Year* yearh = nullptr;
-    Year* year_cur = nullptr;
-    Course* courseh = nullptr; 
     create_new_year(yearh);
-    CreateSem(yearh, year_cur);
-    //PrintSem(yearh, year_cur);
 }
-
 
 /*void PrintSem(Year* yearh, Year*& year_cur)
 {
@@ -45,7 +41,6 @@ void Menu_Op()
 
 void create_new_year(Year*& yearh) {
     string newyear;
-    Year* yearcur = nullptr;
     int offset = 30;
     createframe();
     HANDLE hConsole;
@@ -58,15 +53,14 @@ void create_new_year(Year*& yearh) {
     yearcur->year_name = newyear;
     yearcur->Next = yearh;
     yearh = yearcur;
+    // Create Semester
+    Year* year_cur = Find_Year(yearh, newyear);
+    CreateSem(yearh, year_cur);
 }
 
 void CreateSem(Year*& yearh, Year*& year_cur)
 {
     int num_sem;
-    string name_year;
-    cout << "Input name of year: " << endl;
-    cin >> name_year;
-    year_cur = Find_Year(yearh, name_year);
     cout << "Input the sem you want to create: " << endl;
     cin >> num_sem;
     if (num_sem == 1) year_cur->sem[num_sem - 1].sem_name = "Semester 1";
@@ -76,51 +70,60 @@ void CreateSem(Year*& yearh, Year*& year_cur)
     cin >> year_cur->sem[num_sem - 1].dateStart;
     cout << "Input the end of semester " << num_sem << ":";
     cin >> year_cur->sem[num_sem - 1].dateEnd;
+    //Create Course
+    year_cur->sem[num_sem - 1].courseh = nullptr;
+    CreateCourse(yearh, year_cur, year_cur->sem[num_sem - 1].courseh, num_sem);
 }
 
-void CreateCourse(Year*& yearh, Year*& year_cur, Course*& courseh)
+void CreateCourse(Year*& yearh, Year*& year_cur, Course*& courseh, int num_sem)
 {
-    int num_sem;
-    string name_year;
-    cout << "Input name of year: " << endl;
-    cin >> name_year;
-    year_cur = Find_Year(yearh, name_year);
-    cout << "Input the sem you want to choose: " << endl;
-    cin >> num_sem;
-    Course* coursecur;
-    if (courseh == nullptr)
+    int n;
+    cout << "Input the number of courses you want to create: ";
+    cin >> n;
+    Course* coursecur = nullptr;
+    for (int i = 0; i < n; i++)
     {
-        courseh = new Course;
-        coursecur = courseh;
+        if (courseh == nullptr)
+        {
+            courseh = new Course;
+            coursecur = courseh;
+        }
+        else
+        {
+            coursecur->Next = new Course;
+            coursecur = coursecur->Next;
+        }
+        coursecur->maxnum_stu = 50;
+        cout << "Input name of the course: " << endl;
+        cin >> coursecur->course_name;
+        cout << "Input ID of the course: " << endl;
+        cin >> coursecur->course_id;
+        cout << "Input name of teacher of the course: " << endl;
+        cin >> coursecur->teacher_name;
+        cout << "Input the number of credits of the course: " << endl;
+        cin >> coursecur->cre_num;
+        cout << "Input the registration start day of the course: " << endl;
+        cin >> coursecur->startdate;
+        cout << "Input the registration end day of the course: " << endl;
+        cin >> coursecur->enddate;
     }
-    else
+    //Create Session
+    string course_id;
+    for (int i = 0; i < n; i++)
     {
-        coursecur->Next = new Course;
-        coursecur = coursecur->Next;
+        cout << "Input course ID you want to create sessions: " << endl;
+        cin >> course_id;
+        Course* course_cur = Find_Course(year_cur, courseh, course_id, num_sem);
+        CreateSes(yearh, course_cur, num_sem);
     }
-    year_cur->sem[num_sem - 1].courseh->maxnum_stu = 50;
-    cout << "Input name of the course: " << endl;
-    cin >> year_cur->sem[num_sem - 1].courseh->course_name;
-    cout << "Input ID of the course: " << endl;
-    cin >> year_cur->sem[num_sem - 1].courseh->course_id;
-    cout << "Input name of teacher of the course: " << endl;
-    cin >> year_cur->sem[num_sem - 1].courseh->teacher_name;
-    cout << "Input the number of credits of the course: " << endl;
-    cin >> year_cur->sem[num_sem - 1].courseh->cre_num;
-    cout << "Input the day of the course: " << endl;
-    cin >> year_cur->sem[num_sem - 1].courseh->dateofWeek;
-    cout << "Input the registration start day of the course: " << endl;
-    cin >> year_cur->sem[num_sem - 1].courseh->startdate;
-    cout << "Input the registration end day of the course: " << endl;
-    cin >> year_cur->sem[num_sem - 1].courseh->enddate;
 }
 
-void CreateSes(Year* yearh, Year* year_cur, Course*& courseh, Course* course_cur, int num_sem)
+void CreateSes(Year* yearh, Course*& course_cur, int num_sem)
 {
     for(int i = 0; i < 2; i++)
     {
-        cout << "Input the course's day: " << endl;
-        cin >> year_cur->sem[num_sem - 1].courseh->ses[i].date;
+        cout << "Input the course's day in week: " << endl;
+        cin >> course_cur->ses[i].date;
         cout << "Input the session: " << endl;
         cout << "S1. 7:30" << endl;
         cout << "S2. 9:30" << endl;
@@ -128,8 +131,8 @@ void CreateSes(Year* yearh, Year* year_cur, Course*& courseh, Course* course_cur
         cout << "S4. 15:30" << endl;
         do
         {
-            cin >> year_cur->sem[num_sem - 1].courseh->ses[i].timeofSes;
-        } while (year_cur->sem[num_sem - 1].courseh->ses[i].timeofSes != "S1" && year_cur->sem[num_sem - 1].courseh->ses[i].timeofSes != "S2" && year_cur->sem[num_sem - 1].courseh->ses[i].timeofSes != "S3" && year_cur->sem[num_sem - 1].courseh->ses[i].timeofSes != "S4");
+            cin >> course_cur->ses[i].timeofSes;
+        } while (course_cur->ses[i].timeofSes != "S1" && course_cur->ses[i].timeofSes != "S2" && course_cur->ses[i].timeofSes != "S3" && course_cur->ses[i].timeofSes != "S4");
     }   
 }
 
